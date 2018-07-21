c799b281a258869860b63a52413f1c946e4801cb
Андрей <andrei@MacBook-Pro-Andrej.local>
2018-07-22 00:14:46 +0300

    Fixed tests for Query and Operand classes, removed integration tests, added unit tests

--- tests/test_operand.py
+++ tests/test_operand.py
@@ -1,3 +1,4 @@
+import mock
 import unittest
 
 from models import Model, Field
@@ -16,6 +17,7 @@ class TestOperand(unittest.TestCase):
         operand = Operand(Condition(TestModel.test_field_one,
                                     TestModel.test_field_two, Operator('=')))
         self.assertTrue(operand.is_condition())
+
         operand = Operand(TestModel.test_field_one)
         self.assertFalse(operand.is_condition())
 
@@ -23,18 +25,30 @@ class TestOperand(unittest.TestCase):
         operand = Operand(Condition(TestModel.test_field_one,
                                     TestModel.test_field_two, Operator('=')))
         self.assertFalse(operand.is_field())
+
         operand = Operand(TestModel.test_field_one)
         self.assertTrue(operand.is_field())
 
-    def test_to_str(self):
+    @mock.patch('query.condition.Condition.to_str')
+    @mock.patch('models.field.Field.to_str')
+    def test_to_str(self, field_to_str, condition_to_str):
+        condition_to_str.return_value = 'test_condition'
+        field_to_str.return_value = 'test_field'
+
         operand = Operand(Condition(TestModel.test_field_one,
                           TestModel.test_field_two, Operator('=')))
-        self.assertEqual(operand.to_str(), '(testmodel.test_field_one = testmodel.test_field_two)')
+        self.assertEqual(operand.to_str(), 'test_condition')
+        self.assertTrue(condition_to_str.called)
+
         operand = Operand(TestModel.test_field_one)
-        self.assertEqual(operand.to_str(), 'testmodel.test_field_one')
+        self.assertEqual(operand.to_str(), 'test_field')
+        self.assertTrue(field_to_str.called)
+
         operand = Operand(1)
         self.assertEqual(operand.to_str(), '1')
+
         operand = Operand('1')
         self.assertEqual(operand.to_str(), '"1"')
+
         operand = Operand(None)
         self.assertEqual(operand.to_str(), '')
--- tests/test_query.py
+++ tests/test_query.py
@@ -2,7 +2,7 @@ import mock
 import unittest
 
 from models import Model, Field
-from factory.postgres_factory import PostgresFactory
+from backends.postgresql.postgresql_query import PostgreSQLQuery
 
 
 class TestModel(Model):
@@ -15,65 +15,54 @@ class TestModelTwo(Model):
     test_field_two = Field()
 
 
-@mock.patch('factory.factory', PostgresFactory())
 class TestPostrgreSQLQuery(unittest.TestCase):
-    def test_select(self):
-        query = TestModel.select().sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel')
+    def setUp(self):
+        self.query = PostgreSQLQuery()
+        self.query._query_builder = mock.patch(
+            'backends.postgresql.postgresql_builder.PostgreSQLQueryBuilder').start()
 
-        query = TestModel.select(TestModel.test_field_one).sql().rstrip()
-        self.assertEqual(query, 'SELECT testmodel.test_field_one FROM testmodel')
+    def test_select(self):
+        query = self.query.select()
+        self.query._query_builder.add_fields.return_value = None
+        self.assertTrue(self.query._query_builder.add_fields.called)
+        self.assertEqual(query, self.query)
 
     def test_filter(self):
-        query = TestModel.filter(TestModel.test_field_one == 2).sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel WHERE (testmodel.test_field_one = 2)')
+        query = self.query.filter(TestModel.test_field_one == TestModel.test_field_two)
+        self.query._query_builder.add_conditions.return_value = None
+        self.assertTrue(self.query._query_builder.add_conditions.called)
+        self.assertEqual(query, self.query)
 
-        query = TestModel.filter(TestModel.test_field_one == 2, TestModel.test_field_two == 3).sql().rstrip()
-        self.assertIn(query, ['SELECT * FROM testmodel '
-                              'WHERE (testmodel.test_field_one = 2) AND (testmodel.test_field_two = 3)',
-                              'SELECT * FROM testmodel '
-                              'WHERE (testmodel.test_field_two = 3) AND (testmodel.test_field_one = 2)'])
+    @mock.patch('backends.postgresql.postgresql_query.PostgreSQLQuery._join')
+    def test_outer_join(self, _join):
+        _join.return_value = None
+        query = self.query.outer_join(TestModel)
+        self.assertTrue(_join.called)
+        self.assertEqual(query, self.query)
 
-        query = TestModel.filter(TestModelTwo.test_field_two == 2).sql().rstrip()
-        self.assertIn(query, ['SELECT * FROM testmodel, testmodeltwo '
-                              'WHERE (testmodeltwo.test_field_two = 2)',
-                              'SELECT * FROM testmodeltwo, testmodel '
-                              'WHERE (testmodeltwo.test_field_two = 2)'])
+    @mock.patch('backends.postgresql.postgresql_query.PostgreSQLQuery._join')
+    def test_join(self, _join):
+        _join.return_value = None
+        query = self.query.join(TestModel)
+        self.assertTrue(_join.called)
+        self.assertEqual(query, self.query)
 
-    def test_outer_join(self):
-        query = TestModel.select().outer_join(
-            TestModelTwo, on=TestModelTwo.test_field_two == TestModel.test_field_one).sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel '
-                                'OUTER JOIN testmodeltwo ON (testmodeltwo.test_field_two = testmodel.test_field_one)')
+    @mock.patch('backends.postgresql.postgresql_query.PostgreSQLQuery._join')
+    def test_left_join(self, _join):
+        _join.return_value = None
+        query = self.query.left_join(TestModel)
+        self.assertTrue(_join.called)
+        self.assertEqual(query, self.query)
 
-    def test_join(self):
-        query = TestModel.select().join(
-            TestModelTwo, on=TestModelTwo.test_field_two == TestModel.test_field_one).sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel '
-                                'INNER JOIN testmodeltwo ON (testmodeltwo.test_field_two = testmodel.test_field_one)')
-
-    def test_left_join(self):
-        query = TestModel.select().left_join(
-            TestModelTwo, on=TestModelTwo.test_field_two == TestModel.test_field_one).sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel '
-                                'LEFT JOIN testmodeltwo ON (testmodeltwo.test_field_two = testmodel.test_field_one)')
-
-    def test_right_join(self):
-        query = TestModel.select().right_join(
-            TestModelTwo, on=TestModelTwo.test_field_two == TestModel.test_field_one).sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel '
-                                'RIGHT JOIN testmodeltwo ON (testmodeltwo.test_field_two = testmodel.test_field_one)')
+    @mock.patch('backends.postgresql.postgresql_query.PostgreSQLQuery._join')
+    def test_right_join(self, _join):
+        _join.return_value = None
+        query = self.query.left_join(TestModel)
+        self.assertTrue(_join.called)
+        self.assertEqual(query, self.query)
 
     def test_order_by(self):
-        query = TestModel.select().order_by(TestModel.test_field_one).sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel ORDER BY testmodel.test_field_one')
-
-        query = TestModel.select().order_by(TestModel.test_field_one, TestModel.test_field_two).sql().rstrip()
-        self.assertIn(query, ['SELECT * FROM testmodel '
-                              'ORDER BY testmodel.test_field_one, testmodel.test_field_two',
-                              'SELECT * FROM testmodel '
-                              'ORDER BY testmodel.test_field_two, testmodel.test_field_one'])
-
-        query = TestModel.select().order_by(TestModel.test_field_one.desc()).sql().rstrip()
-        self.assertEqual(query, 'SELECT * FROM testmodel '
-                                'ORDER BY testmodel.test_field_one DESC')
+        query = self.query.order_by(TestModel.test_field_two)
+        self.query._query_builder.add_ordering_fields.return_value = None
+        self.assertTrue(self.query._query_builder.add_ordering_fields.called)
+        self.assertEqual(query, self.query)
