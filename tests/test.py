b63e8fb0e06f314e12cd8dc2b21fa567e9967b9a
Andrei <chubarkin.andrey@gmail.com>
2018-08-01 20:05:16 +0300

    Added logic for factory

--- tests/test.py
+++ tests/test.py
@@ -2,12 +2,12 @@ import os
 import unittest
 import sys
 
-from elysium import config
-from elysium.tests.test_backends.test_postgresql.test_config import db_config
 
 if __name__ == '__main__':
     sys.path.append(os.getcwd())
 
+    from elysium import config
+    from elysium.tests.test_backends.test_postgresql.test_config import db_config
     config.update(db_config)
 
     testsuite = unittest.TestLoader().discover('./elysium/tests')
